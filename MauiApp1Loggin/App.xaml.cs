b7e1f8f808e3794b10f0a59691678032e250a273
Carlos <Carlos@CARLOSPC>
2024-11-14 08:52:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1Loggin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1Loggin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
